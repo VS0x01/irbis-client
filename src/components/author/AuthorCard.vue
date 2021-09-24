7209b3b5a814bf35b4ee6d645c5966c5d8c01e51
Vadim Shesterikov <vadim.a.shesterikov@gmail.com>
2021-09-24 13:35:37 +0300

    AuthorCard: update markup

--- src/components/author/AuthorCard.vue
+++ src/components/author/AuthorCard.vue
@@ -1,76 +1,81 @@
 <template>
   <v-card :loading="!author">
     <v-skeleton-loader type="card-heading" v-if="!author" />
-    <v-container v-if="!author">
-      <v-row v-for="n in 4" :key="n" align="center" justify="start">
-        <v-col>
-          <v-skeleton-loader type="card-heading" width="100%" />
-        </v-col>
-        <v-col>
-          <v-skeleton-loader type="text" />
-        </v-col>
-        <v-spacer />
-      </v-row>
-      <v-row>
-        <v-col class="flex-grow-0" v-for="n in 4" :key="n">
-          <v-skeleton-loader type="chip" />
-        </v-col>
-      </v-row>
-    </v-container>
+    <v-simple-table v-if="!author">
+      <template v-slot:default>
+        <tbody>
+          <tr v-for="n in 4" :key="n">
+            <td><v-skeleton-loader type="card-heading" /></td>
+            <td><v-skeleton-loader type="text" max-width="50%" /></td>
+          </tr>
+          <tr>
+            <td><v-skeleton-loader type="card-heading" /></td>
+            <td>
+              <v-skeleton-loader
+                type="chip@4"
+                class="d-flex justify-space-between"
+                max-width="50%"
+              />
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
 
     <v-card-title v-if="author">
       {{ author.lastName + ' ' + author.firstName }}
     </v-card-title>
-    <v-container v-if="author">
-      <v-row align="center" v-if="author.academicStatus">
-        <v-col>
-          <v-card-title>Вчене звання:</v-card-title>
-        </v-col>
-        <v-col>
-          <v-card-text>{{ author.academicStatus }}</v-card-text>
-        </v-col>
-      </v-row>
-
-      <v-row class="d-flex" align="center" v-if="author.degree">
-        <v-col>
-          <v-card-title>Науковий ступінь:</v-card-title>
-        </v-col>
-        <v-col>
-          <v-card-text>{{ author.degree }}</v-card-text>
-        </v-col>
-      </v-row>
-
-      <v-row class="d-flex" align="center" v-if="author.workPlace">
-        <v-col>
-          <v-card-title>Місце роботи:</v-card-title>
-        </v-col>
-        <v-col>
-          <v-card-text>{{ author.workPlace }}</v-card-text>
-        </v-col>
-      </v-row>
-
-      <v-row class="d-flex" v-if="Object.keys(author.links).includes('KNEU')">
-        <v-col>
-          <v-card-title>
-            Сторінка науковця на сайті університету:
-          </v-card-title>
-        </v-col>
-        <v-col>
-          <v-card-text>{{ author.academicStatus }}</v-card-text>
-        </v-col>
-      </v-row>
-
-      <v-card-title v-if="Object.keys(author.links).length > 0">
-        Посилання:
-      </v-card-title>
-      <v-card-text>
-        <v-chip-group>
-          <v-chip link v-for="(v, k) in author.links" :key="k" :href="v">
-            {{ k }}
-          </v-chip>
-        </v-chip-group>
-      </v-card-text>
-    </v-container>
+    <v-simple-table v-if="author">
+      <template v-slot:default>
+        <tbody>
+          <tr>
+            <td><v-card-title>Вчене звання:</v-card-title></td>
+            <td>
+              <v-card-text>{{ author.academicStatus }}</v-card-text>
+            </td>
+          </tr>
+          <tr>
+            <td><v-card-title>Науковий ступінь:</v-card-title></td>
+            <td>
+              <v-card-text>{{ author.degree }}</v-card-text>
+            </td>
+          </tr>
+          <tr>
+            <td><v-card-title>Місце роботи:</v-card-title></td>
+            <td>
+              <v-card-text>{{ author.workPlace }}</v-card-text>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <v-card-title>
+                Сторінка науковця на сайті університету:
+              </v-card-title>
+            </td>
+            <td>
+              <v-card-text>{{ author.academicStatus }}</v-card-text>
+            </td>
+          </tr>
+          <tr v-if="Object.keys(author.links).length > 0">
+            <td><v-card-title> Посилання: </v-card-title></td>
+            <td>
+              <v-card-text>
+                <v-chip-group>
+                  <v-chip
+                    link
+                    v-for="(v, k) in author.links"
+                    :key="k"
+                    :href="v"
+                  >
+                    {{ k }}
+                  </v-chip>
+                </v-chip-group>
+              </v-card-text>
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
   </v-card>
 </template>
 
